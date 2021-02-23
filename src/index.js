33e6142c30c17557fcd1a81d40cebc805db62a5e
rumbie <rumbiemudzie@gmail.com>
2021-02-23 11:44:58 +0200

    Set Phaser with config object

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
