00f189e4c7ab1c6b63b26ec22c8bee1b7588b1e3
rumbie <rumbiemudzie@gmail.com>
2021-03-06 22:08:42 +0200

    Import UserInputScene

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Phaser from 'phaser';
 
 import { Laser, LaserGroup,  SpaceScene, } from './scenes/SpaceScene';
+import UserInputScene from './scenes/UserInputScene';
 import LoadingScene from './scenes/LoadingScene';
 import GameOverScene from './scenes/GameOverScene';
 
 const config = {
   type: Phaser.AUTO,
+  parent: "game",
   width: 800,
   height: 600,
   dom: {
@@ -19,7 +21,7 @@ const config = {
       enableBody: true
     }
   },
-  scene: [ LoadingScene, SpaceScene, GameOverScene]
+  scene: [ UserInputScene, LoadingScene, SpaceScene, GameOverScene]
   
 };
 
