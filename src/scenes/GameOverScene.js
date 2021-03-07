5c83fa13eb5c7c1115540d00b2bcf0206525cfcc
rumbie <rumbiemudzie@gmail.com>
2021-03-07 23:16:01 +0200

    Update with the playerName property to gameScore object

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -1,24 +1,27 @@
-import Phaser from 'phaser';
-import {gameScore} from './SpaceScene'
+import Phaser from 'phaser'
 
+import { gameScore } from './UserInputScene';
 class GameOverScene extends Phaser.Scene {
-  constructor() {
-    super('GameOverScene');
+  constructor () {
+    super('GameOverScene')
   }
-  
-  preload() {
-    this.load.image('gameOverBg', '/assets/images/gameoverbg.png');
+  preload () {
+    this.load.image('gameOverBg', '/assets/images/gameoverbg.png')
   }
-
-
-  create() {
-      let gameOverBackground = this.add.image(0, 0, 'gameOverBg').setOrigin(0, 0);
-      this.gameOverText = this.add.text(200, 300, 'Game Over \n Your Score: ' + gameScore.score,  {
-      fontSize: '40px', fill: '#fff'
-    })
-    gameScore.score = 0;
+  create () {
+    this.form = document.querySelector('#input-form')
+    this.form.style.display = 'none'
+    this.add.image(0, 0, 'gameOverBg').setOrigin(0, 0)
+    this.gameOverText = this.add.text(
+      200,
+      300,
+      `Game Over ${gameScore.playerName} \n Your Score: ${gameScore.score}`,
+      {
+        fontSize: '40px',
+        fill: '#fff'
+      }
+    )
+    gameScore.score = 0
   }
-
 }
-
 export default GameOverScene;
