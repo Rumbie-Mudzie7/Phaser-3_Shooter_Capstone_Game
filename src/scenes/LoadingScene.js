0fe73b2277445010915a2495aa2366d5898f1acf
rumbie <rumbiemudzie@gmail.com>
2021-03-15 22:14:18 +0200

    Add and load assets from other scene files

--- src/scenes/LoadingScene.js
+++ src/scenes/LoadingScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { gameScore} from './UserInputScene';
 
 class LoadingScene extends Phaser.Scene {
   constructor() {
@@ -10,10 +11,7 @@ class LoadingScene extends Phaser.Scene {
     const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
-    this.load.image('logo', '/assets/images/galaxy4.png');
-    for (let i = 0; i < 100; i += 1) {
-      this.load.image(`logo${i}`, '/assets/images/galaxy4.png');
-    }
+    
     this.load.on('progress', (value) => {
       percentText.setText(`${parseInt(value * 100)}%`);
       progressBar.clear();
@@ -50,17 +48,26 @@ class LoadingScene extends Phaser.Scene {
       },
     });
     loadingText.setOrigin(0.5, 0.5);
+    this.load.image('logo', '/assets/images/galaxy4.png');
+    this.load.image('button1', '/assets/images/green_button01.png');
+    this.load.image('button2', '/assets/images/green_button02.png');
+    this.load.image('box', '/assets/images/grey_box.png');
+    this.load.image('checkedBox', '/assets/images/green_boxCheckmark.png');
+    this.load.audio('bgMusic', ['/assets/sounds/bgMusic.mp3']);
+    this.load.image('gameOverBg', '/assets/images/gameoverbg.png');
+
   }
 
   create() {
     const { width } = this.scale;
     const { height } = this.scale;
+    gameScore.backGroundMc = this.sound.add('bgMusic', {loop:true});
     this.add.text(width / 2, height / 2 - 200, 'Click Anywhere to Start Game', {
       font: '20px monospace',
       fill: '#ffffff',
     }).setOrigin(0.5, 0.5);
     const logo = this.add.image(this.scale.width / 2, this.scale.height / 2, 'logo').setScale(0.5);
-    this.input.on('pointerdown', () => this.scene.start('UserInputScene'));
+    this.input.on('pointerdown', () => this.scene.start('TitleScene'));
   }
 }
 export default LoadingScene;
