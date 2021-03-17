40a790b2491e33db696143ce2e7d6b89cff44a3b
rumbie <rumbiemudzie@gmail.com>
2021-03-17 17:06:25 +0200

    Run eslint validator

--- src/__tests__/request.test.js
+++ src/__tests__/request.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import 'babel-polyfill';
 import { putScore, getScores } from '../api/request';
 
@@ -23,41 +24,23 @@ const response = {
 describe('Getscores ', () => {
   it('Returns an array of objects', () => {
     fetch.mockResponse(response);
-    (async () => {
+    async () => {
       const result = await getScores();
       return expect(result).toEqual(response.result);
-    })();
+    };
   });
 });
 
 describe('putScore', () => {
   it('Sends user data to the server', () => {
     fetch.mockResponse();
-    (async () => {
+    async () => {
       const player = {
         user: 'Lynette',
         score: 890,
       };
       const response = await putScore(player.user, player.score);
       return expect(response.result).toEqual('Leaderboard score updated successfully');
-    })();
+    };
   });
 });
-
-
-// describe('create a game with a valid name', () => {
-//   it('create a game to if the name is valid', () => {
-//     createGame().then((data) => {
-//       expect(data).toBeTruthy();
-//     });
-//   });
-// });
-
-
-// describe('retrieve the score', () => {
-//   it('return the score if exists', () => {
-//     getScores().then((data) => {
-//       expect(typeof data).toBe('object');
-//     });
-//   });
-// });
