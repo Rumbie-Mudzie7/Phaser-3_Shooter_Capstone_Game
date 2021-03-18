fe3b17588dc974af0d191584b85637b37c33638f
rumbie <rumbiemudzie@gmail.com>
2021-03-18 08:48:54 +0200

    Configure css with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
   module: {
     rules: [
       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
